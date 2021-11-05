8bd48a870a139d91f969f2989d9bcefca960a683
Joerg Rosenkranz <joerg.rosenkranz@quest.com>
2021-11-05 09:48:39 +0100

    Add Windows Terminal settings

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+# History handling
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle ListView
+    Set-PSReadLineOption -EditMode Windows
+}
+
+# Git handling
+Import-Module posh-git
+# $env:POSH_GIT_ENABLED = $true
+
+# Init oh-my-posh
+oh-my-posh --init --shell pwsh --config ~/.powershell/oh-my-posh.json | Invoke-Expression
+
+# Icons in dir
+Import-Module -Name Terminal-Icons
+
