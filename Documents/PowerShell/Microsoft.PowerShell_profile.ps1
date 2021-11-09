6afee78a1a116de5ccf828342cf0012846e92ca8
Joerg Rosenkranz <joerg.rosenkranz@quest.com>
2021-11-09 09:54:29 +0100

    Consolidate PowerShell files

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -13,7 +13,7 @@ Import-Module posh-git
 # $env:POSH_GIT_ENABLED = $true
 
 # Init oh-my-posh
-oh-my-posh --init --shell pwsh --config ~/.powershell/oh-my-posh.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config $PSScriptRoot\oh-my-posh.json | Invoke-Expression
 
 # Icons in dir
 Import-Module -Name Terminal-Icons
