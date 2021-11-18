7ca10f251d74b6846b76e395f5b4674e90e22c71
Joerg Rosenkranz <joerg.rosenkranz@quest.com>
2021-11-18 09:26:06 +0100

    cb and change parameter handling

--- Documents/PowerShell/aliases.ps1
+++ Documents/PowerShell/aliases.ps1
@@ -1,11 +1,11 @@
 # Alias definitions
 
 function MsBuild2019 {
-    & "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" -nologo -m -P:VI_PROJECTDIR="$(git rev-parse --show-toplevel)" $args
+    & "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" -nologo -m -P:VI_PROJECTDIR="$(git rev-parse --show-toplevel)" @args
 }
 
 function MsBuild2022 {
-    & "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" -nologo -m -P:VI_PROJECTDIR="$(git rev-parse --show-toplevel)" $args
+    & "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" -nologo -m -P:VI_PROJECTDIR="$(git rev-parse --show-toplevel)" @args
 }
 
 function Get-GitRoot () {
@@ -18,7 +18,7 @@ Set-Alias -Name msbuildpreview -Value MsBuild2022
 function Alias-Hierarchy {
     $root = Get-GitRoot
     $current = $(git rev-parse --show-prefix).Replace('/', '\').Replace('.', '_').TrimEnd('\')
-    msbuild "$root\Build\Hierarchy.proj" "-P:PostBuildEvent=" "-T:$current" $args
+    msbuild "$root\Build\Hierarchy.proj" "-P:PostBuildEvent=" "-T:$current" @args
 }
 Set-Alias -Name hierarchy -Value Alias-Hierarchy
 Set-Alias -Name buildrel -Value Alias-Hierarchy
@@ -26,39 +26,39 @@ Set-Alias -Name buildrel -Value Alias-Hierarchy
 function Alias-HierarchyPreview {
     $root = Get-GitRoot
     $current = $(git rev-parse --show-prefix).Replace('/', '\').Replace('.', '_').TrimEnd('\')
-    msbuildpreview "$root\Build\Hierarchy.proj" "-P:PostBuildEvent=" "-T:$current" $args
+    msbuildpreview "$root\Build\Hierarchy.proj" "-P:PostBuildEvent=" "-T:$current" @args
 }
 Set-Alias -Name hierarchy -Value Alias-Hierarchy
 
 function Alias-BuildDbg {
-    hierarchy "-P:VICFG=DEBUG" "-P:BUILD_WEB=no" $args
+    hierarchy "-P:VICFG=DEBUG" "-P:BUILD_WEB=no" @args
 }
 Set-Alias -Name builddbg -Value Alias-BuildDbg
 
 function Alias-BuildDbgPreview {
-    hierarchypreview "-P:VICFG=DEBUG" "-P:BUILD_WEB=no" $args
+    hierarchypreview "-P:VICFG=DEBUG" "-P:BUILD_WEB=no" @args
 }
 Set-Alias -Name builddbgpreview -Value Alias-BuildDbgPreview
 
 function Alias-BuildDbgWeb {
-    hierarchy "-P:VICFG=DEBUG" $args
+    hierarchy "-P:VICFG=DEBUG" @args
 }
 Set-Alias -Name builddbgweb -Value Alias-BuildDbgWeb
 
 function Alias-All {
     $root = Get-GitRoot
-    msbuild "$root\Build\Build.proj" "-P:PostBuildEvent=" "-nodeReuse:False" $args
+    msbuild "$root\Build\Build.proj" "-P:PostBuildEvent=" "-nodeReuse:False" @args
 }
 Set-Alias -Name all -Value Alias-All
 Set-Alias -Name allrel -Value Alias-All
 
 function Alias-AllDbg {
-    all "-P:VICFG=DEBUG" "-P:BUILD_WEB=no" "-P:BuildInParallel=true" $args
+    all "-P:VICFG=DEBUG" "-P:BUILD_WEB=no" "-P:BuildInParallel=true" @args
 }
 Set-Alias -Name alldbg -Value Alias-AllDbg
 
 function Alias-AllDbgWeb {
-    all "-P:VICFG=DEBUG" "-P:BuildInParallel=true" $args
+    all "-P:VICFG=DEBUG" "-P:BuildInParallel=true" @args
 }
 Set-Alias -Name alldbgweb -Value Alias-AllDbgWeb
 
@@ -90,16 +90,54 @@ function Get-GitLastBranch {
 
 function Alias-RemoveLastBranch {
     $lastBranch = Get-GitLastBranch
-    git branch -d "$lastBranch" $args
+    git branch -d "$lastBranch" @args
 }
 Set-Alias -Name rm-lastbranch -Value Alias-RemoveLastBranch
 
 function Run-Mono {
     $P = $PWD.Path.Replace('\', '/')
-    docker run -it --rm -v "$($P):/asm" -w /asm mono:latest $args
+    docker run -it --rm -v "$($P):/asm" -w /asm mono:latest @args
 }
 
 function Run-Core {
     $P = $PWD.Path.Replace('\', '/')
-    docker run -it --rm -v "$($P):/asm" -w /asm mcr.microsoft.com/dotnet/core/runtime:latest $args
+    docker run -it --rm -v "$($P):/asm" -w /asm mcr.microsoft.com/dotnet/core/runtime:latest @args
 }
+
+
+# function _WorkTreeCompleter{
+#     param ( $commaName,
+#             $parameterName,
+#             $wordToComplete,
+#             $commandAst,
+#             $fakeBoundParameters )
+
+#     $(& git worktree list | % { $_.Split(' ')[0] })
+# }
+
+function Change-Branch {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Branch
+    )
+
+	if ( ! $Branch ) {
+		Write-Output "You need to provide a branch name"
+		return 1
+    }
+
+	$BranchRoot = $(& git worktree list | select-string "[$Branch]" -SimpleMatch | % { $_.Line.Split(' ')[0] })
+	$RelativePath="$(& git rev-parse --show-prefix)"
+
+	$NewPath = "$BranchRoot\$RelativePath"
+
+	Set-Location "$NewPath"
+}
+
+# Microsoft.PowerShell.Core\Register-ArgumentCompleter -CommandName Change-Branch -ParameterName Branch -ScriptBlock {
+#     param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams)
+
+#     $(& git worktree list | select-string "$WordToComplate" -SimpleMatch | % { $_.Line.Split(' ')[0] })
+# }
+
+Set-Alias -Name cb -Value Change-Branch
